07d7f63e6af3723195a58baace9c9c7d01d3d2b6
astefanczyk <astefanczyk@student.agh.edu.pl>
2024-05-25 13:24:55 +0200

    load data

--- src/load_data.py
+++ src/load_data.py
@@ -42,6 +42,9 @@ def apply_mild_distortion(image):
     img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2HSV)
     img_np[..., 1] = img_np[..., 1] // random.uniform(1.2, 1.5)
     img_np = cv2.cvtColor(img_np, cv2.COLOR_HSV2RGB)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), random.randint(10, 70)]
+    _, encimg = cv2.imencode('.jpg', img_np, encode_param)
+    img_np = cv2.imdecode(encimg, 1)
     return Image.fromarray(img_np)
 
 def apply_background_distortion(image):
@@ -50,14 +53,14 @@ def apply_background_distortion(image):
     blurred = cv2.GaussianBlur(gray, (15, 15), 0)
     _, mask = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
     mask_inv = cv2.bitwise_not(mask)
-    bg = cv2.GaussianBlur(img_np, (21, 21), 50)
+    bg = cv2.GaussianBlur(img_np, (25, 25), 50)
     fg = cv2.bitwise_and(img_np, img_np, mask=mask)
     combined = cv2.add(bg, fg)
     return Image.fromarray(combined)
 
 def apply_severe_distortion(image):
     img_np = np.array(image)
-    noise_level = random.randint(0, 2)  # Higher noise level for severe distortion
+    noise_level = random.randint(0, 1)  # Higher noise level for severe distortion
     noise = np.random.normal(0, noise_level, img_np.shape).astype(np.uint8)
     img_np = cv2.add(img_np, noise)
     img_np = np.clip(img_np, 0, 255).astype(np.uint8)
