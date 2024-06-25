c559a036de21ff03b3c5611aa01f084caac164aa
astefanczyk <astefanczyk@student.agh.edu.pl>
2024-06-25 21:15:20 +0200

    removing redundant code

--- src/config.py
+++ src/config.py
@@ -2,11 +2,8 @@ from ypstruct import struct
 
 dataset_config = struct()
 
-dataset_config.number_of_images = 100
-dataset_config.img_dir = '../data/Humans'
-
-dataset_config.vid_original_dir = '..\\data\\videos\\original'
-dataset_config.vid_compressed_dir = '..\\data\\videos\\compressed'
+dataset_config.vid_original_dir = '../data/videos/original'
+dataset_config.vid_compressed_dir = '../data/videos/compressed'
 
 training_config = struct()
 
