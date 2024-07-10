278a152bf0d9164a924abe555029558e3e9ffc93
Artur Stefanczyk <as.stefanczyk@gmail.com>
2024-07-10 13:39:49 +0200

    complete detection

--- src/modes/detect.py
+++ src/modes/detect.py
@@ -1,18 +1,42 @@
+import os
 import torch
 from detection import MTNetwork
-
+from PIL import Image
 
 class Detect:
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    mt_network = MTNetwork()        
+    mt_network = MTNetwork()
 
     def detect(self, image_path, threshold):
-        
         probability = self.mt_network.detect(image_path)
+        if probability:
+            is_detectable = probability > threshold
+        else:
+            print('No face detected')
+            is_detectable = False
+        print(f'Image: {image_path}')
+        print(f'Probability of detected face: {probability}')
+        print(f'Is face detectable (with given threshold): {is_detectable}')
 
-        is_detectable = probability > threshold
-
-        print(f'probability of detected face: {probability}')
-        print(f'is face detectable(with given threshold): {is_detectable}')
+    def detect_in_directory(self, dir_path, threshold):
+        counter = 0
+        for root, _, files in os.walk(dir_path):
+            for i, file in enumerate(files):
+                if file.lower().endswith(('png', 'jpg', 'jpeg')):
+                    image_path = os.path.join(root, file)
+                    print(image_path)
+                    probability = self.mt_network.detect(image_path)
+                    print(probability)
+                    if probability:
+                        is_detectable = probability > threshold
+                    else:
+                        print('No face detected')
+                        is_detectable = False
+                    if is_detectable: 
+                        counter += 1
+                    print(f'Image: {image_path}')
+                    print(f'Probability of detected face: {probability}')
+                    print(f'Is face detectable (with given threshold): {is_detectable}')
+        print(f'percantage of detectable images: {counter / (i + 1)}')
 
--- src/modes/parser.py
+++ src/modes/parser.py
@@ -12,15 +12,12 @@ from config import (
     training_config
 )
 
-
 class Parser:
 
     def __init__(self):
-
         self.arg_parser = self.__setup_arg_parser()
 
     def __setup_arg_parser(self):
-        
         parser = ArgumentParser()
         training_parser = parser.add_argument_group('Training Mode')
         training_parser.add_argument('-t','--training', action='store_true')
@@ -31,29 +28,28 @@ class Parser:
 
         detect_parser = parser.add_argument_group('Detect Mode')
         detect_parser.add_argument('-d', '--detect', action='store_true')
+        detect_parser.add_argument('--dir_path', type=str, required=False, help='Path to the directory containing images')
         detect_parser.add_argument('--threshold', type=float, required=False, help='Threshold of detection')
- 
+
         return parser
 
     def switch_mode(self):
-       
         args = self.arg_parser.parse_args()
 
-        if args.training: 
+        if args.training:
             Training()
-            
         elif args.predict:
             predict = Predict(training_config.save_model_path)
             prediction = predict.predict(args.image_path)
             print(f'Prediction for given image is:\n{prediction}')
-
         elif args.detect:
             detect = Detect()
-            detect.detect(
-                image_path=args.image_path,
-                threshold=args.threshold
-            )
-
-        else: 
+            if args.dir_path:
+                detect.detect_in_directory(dir_path=args.dir_path, threshold=args.threshold)
+            elif args.image_path:
+                detect.detect(image_path=args.image_path, threshold=args.threshold)
+            else:
+                print('Please provide either an image path or a directory path for detection.')
+        else:
             Training()
 
